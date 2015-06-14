9f2dc62a3b40536af0693d95711ccc9254275ba0
Darkhanis <dbncourt@gmail.com>
2015-06-13 22:46:19 -0600

    Tutorial 2: Creating a Framework and Window
    Simple window made with the Windows API

---
+++ Rastertek/Engine/main.cpp
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: main.cpp
+////////////////////////////////////////////////////////////////////////////////
+
+#include "systemclass.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd){
+
+	SystemClass* System;
+
+	//Create the system object
+	System = new SystemClass;
+	if (!System)
+	{
+		return 0;
+	}
+
+	//Initialize and run the system object
+	if (System->Initialize())
+	{
+		System->Run();
+	}
+
+	//Shutdown and release the system object
+	System->Shutdown();
+	delete System;
+	System = NULL;
+	
+	return 0;
+}
