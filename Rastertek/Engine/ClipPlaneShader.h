bad0eb302041aa907f6576ad31a7816ccf2d9ee8
Daniel Betancourt <dbncourt@gmail.com>
2015-07-30 21:08:53 -0600

    Tutorial 24: Clipping Planes
    Clipping planes using HLSL and C++

---
+++ Rastertek/Engine/ClipPlaneShader.h
@@ -0,0 +1,59 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: ClipPlaneShader.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _CLIPPLANESHADER_H_
+#define _CLIPPLANESHADER_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <d3d11.h>
+#include <d3dx10math.h>
+#include <d3dx11async.h>
+#include <fstream>
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: ClipPlaneShader
+////////////////////////////////////////////////////////////////////////////////
+class ClipPlaneShader
+{
+private:
+	struct MatrixBufferType
+	{
+		D3DXMATRIX world;
+		D3DXMATRIX view;
+		D3DXMATRIX projection;
+	};
+
+	struct ClipPlaneBufferType
+	{
+		D3DXVECTOR4 clipPlane;
+	};
+
+	ID3D11VertexShader* m_vertexShader;
+	ID3D11PixelShader* m_pixelShader;
+	ID3D11InputLayout* m_layout;
+	ID3D11SamplerState* m_sampleState;
+	ID3D11Buffer* m_matrixBuffer;
+	ID3D11Buffer* m_clipPlaneBuffer;
+
+public:
+	ClipPlaneShader();
+	ClipPlaneShader(const ClipPlaneShader& other);
+	~ClipPlaneShader();
+
+	bool Initialize(ID3D11Device* device, HWND hwnd);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext* deviceContext, int indexCount, D3DXMATRIX worldMatrix, D3DXMATRIX viewMatrix, D3DXMATRIX projectionMatrix, ID3D11ShaderResourceView* texture, D3DXVECTOR4 clipPlane);
+
+private:
+	bool InitializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vsFilename, WCHAR* psFilename);
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob* errorMessage, HWND hwnd, WCHAR* shaderFilename);
+
+	bool SetShaderParameters(ID3D11DeviceContext* deviceContext, D3DXMATRIX worldMatrix, D3DXMATRIX viewMatrix, D3DXMATRIX projectionMatrix, ID3D11ShaderResourceView* texture, D3DXVECTOR4 clipPlane);
+	void RenderShader(ID3D11DeviceContext* deviceContext, int indexCount);
+};
+#endif
